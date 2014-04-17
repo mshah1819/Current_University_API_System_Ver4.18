0bf697cbf24f150afa3b7b41f49f4daed60bc3bf
mshah1819 <mshah1819@gmail.com>
2014-04-17 15:00:08 -0700

    Initial Commit
    Current_University_API_System_VerAPR17

---
+++ src/main/java/com/cmpe295/university/model/UniversityInfo2List.java
@@ -0,0 +1,16 @@
+package com.cmpe295.university.model;
+
+import java.util.List;
+
+public class UniversityInfo2List {
+    public List<UniversityInfo2> getUnivInfoList() {
+		return univInfoList;
+	}
+
+	public void setUnivInfoList(List<UniversityInfo2> univInfoList) {
+		this.univInfoList = univInfoList;
+	}
+
+	private List<UniversityInfo2> univInfoList;
+
+}
